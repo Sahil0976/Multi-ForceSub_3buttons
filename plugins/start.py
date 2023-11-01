2d7f78fc6c14a1a3faa1c5a018fe416efb3cfcdb
Nozel Silva <97865856+Sahil0976@users.noreply.github.com>
2023-11-01 09:04:37 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -55,11 +55,11 @@ async def start_command(client: Client, message: Message):
                 ids = [int(int(argument[1]) / abs(client.db_channel.id))]
             except:
                 return
-        temp_msg = await message.reply("Please wait...")
+        temp_msg = await message.reply("Wait Dude...")
         try:
             messages = await get_messages(client, ids)
         except:
-            await message.reply_text("Something went wrong..!")
+            await message.reply_text("I Feel like there is Something wrong..!")
             return
         await temp_msg.delete()
 
