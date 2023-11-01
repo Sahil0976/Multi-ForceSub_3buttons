efb5c22c3e5afd4e1d3d420821bbaee8a139478a
Nozel Silva <97865856+Sahil0976@users.noreply.github.com>
2023-11-01 10:50:01 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -43,7 +43,7 @@ class Bot(Client):
 
         if FORCESUB_CHANNEL:
             try:
-                link = (await self.get_chat(FORCE_SUB_CHANNEL)).invite_link
+                link = (await self.get_chat(FORCESUB_CHANNEL)).invite_link
                 if not link:
                     await self.export_chat_invite_link(FORCESUB_CHANNEL)
                     link = (await self.get_chat(FORCESUB_CHANNEL)).invite_link
