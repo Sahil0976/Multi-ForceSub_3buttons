1c1f750ea5715861b5fab8f22007577a889311fd
Nozel Silva <97865856+Sahil0976@users.noreply.github.com>
2023-11-03 11:37:21 +0530

    Update database.py

--- database/database.py
+++ database/database.py
@@ -2,42 +2,34 @@
 #recoded by its_tartaglia_Childe
 
 
-import pymongo
-from pymongo.errors import ServerSelectionTimeoutError
+import pymongo, os
 from config import DB_URI, DB_NAME
 
-async def connect_to_mongodb():
-    try:
-        dbclient = pymongo.MongoClient(DB_URI)
-        database = dbclient[DB_NAME]
-        user_data = database['users']
-        return user_data
-    except ServerSelectionTimeoutError as e:
-        print(f"Error connecting to MongoDB: {e}")
-        return None
 
-async def close_mongodb_connection(client):
-    if client:
-        client.close()
+dbclient = pymongo.MongoClient(DB_URI)
+database = dbclient[DB_NAME]
 
-async def database_operations():
-    client = await connect_to_mongodb()
-    if client:
-        try:
-            # Now you can use the user_data object for your database operations
-            # For example:
-            user_id = 12345
-            await add_user(user_id, client)
-            await present_user(user_id, client)
-            await full_userbase(client)
-            await del_user(user_id, client)
-        finally:
-            await close_mongodb_connection(client)
-    else:
-        # Handle the case where the MongoDB connection could not be established
-        pass
 
-# Usage example
-if __name__ == '__main__':
-    import asyncio
-    asyncio.run(database_operations())
+user_data = database['users']
+
+
+
+async def present_user(user_id : int):
+    found = user_data.find_one({'_id': user_id})
+    return bool(found)
+
+async def add_user(user_id: int):
+    user_data.insert_one({'_id': user_id})
+    return
+
+async def full_userbase():
+    user_docs = user_data.find()
+    user_ids = []
+    for doc in user_docs:
+        user_ids.append(doc['_id'])
+        
+    return user_ids
+
+async def del_user(user_id: int):
+    user_data.delete_one({'_id': user_id})
+    return
